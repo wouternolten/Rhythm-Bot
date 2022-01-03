07dcdedefefc746211c9b4ef6d1b7c1ca1e3c0a8
Herr Scheissmann <wouternolten@gmail.com>
2022-01-03 16:49:56 +0100

    Start refactor of commands

---
+++ src/command/JoinUserChannelCommand.ts
@@ -0,0 +1,31 @@
+import { MediaPlayer } from './../media/media-player';
+import { SuccessfulParsedMessage } from 'discord-command-parser';
+import { Message } from 'discord.js';
+import { ICommand } from './ICommand';
+import { IRhythmBotConfig } from 'src/bot';
+import { createErrorEmbed, createInfoEmbed, joinUserChannel } from '../helpers';
+
+export class JoinUserChannelCommand implements ICommand {
+    constructor(
+        private readonly player: MediaPlayer,
+        private readonly config: IRhythmBotConfig
+    ) {
+        this.player = player;
+        this.config = config;
+    }
+
+    async execute(cmd: SuccessfulParsedMessage<Message>, msg: Message): Promise<void> {
+        joinUserChannel(msg)
+            .then((connection) => {
+                this.player.connection = connection;
+                msg.channel.send(createInfoEmbed(`Joined Channel: ${connection.channel.name}`));
+                if (this.config.auto.play) {
+                    this.player.play();
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                msg.channel.send(createErrorEmbed(err));
+            });
+    }
+}
