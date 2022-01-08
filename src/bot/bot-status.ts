9ad57e49215aa271b5f8fe3ef6d6d01b03ea642d
Herr Scheissmann <wouternolten@gmail.com>
2022-01-08 12:25:10 +0100

    Refactor bot status constructor

--- src/bot/bot-status.ts
+++ src/bot/bot-status.ts
@@ -1,11 +1,7 @@
 import { Client, PresenceStatusData } from 'discord.js';
 
 export class BotStatus {
-    client: Client;
-
-    constructor(client: Client) {
-        this.client = client;
-    }
+    constructor(private readonly client: Client) {}
 
     setBanner(status: string) {
         this.client.user.setPresence({
