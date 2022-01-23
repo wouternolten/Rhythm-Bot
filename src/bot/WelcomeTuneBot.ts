3b45069d7e18d37531afd9983c6343f51899423b
Herr Scheissmann <wouternolten@gmail.com>
2022-01-23 15:13:45 +0100

    Refactor welcome tune bot and add play sound file command

--- src/bot/WelcomeTuneBot.ts
+++ src/bot/WelcomeTuneBot.ts
@@ -1,12 +1,8 @@
+import { PlaySoundFileCommand } from './../command/PlaySoundFileCommand';
 import * as fs from 'fs';
-import {
-    ForcePlayVideoCommand,
-    ICommand,
-} from '../command';
-import { MediaPlayer } from '../media';
-import { BotStatus } from './bot-status';
+import { ICommand } from '../command';
 import { IRhythmBotConfig } from './bot-config';
-import { createErrorEmbed, createInfoEmbed } from '../helpers';
+import { createInfoEmbed } from '../helpers';
 import {
     IBot,
     CommandMap,
@@ -15,15 +11,12 @@ import {
     Interface,
     SuccessfulParsedMessage,
     Message,
-    readFile,
     VoiceConnection,
     VoiceState,
     projectDir,
     requireFile,
 } from 'discord-bot-quickstart';
 
-const helptext = readFile('../helptext.txt');
-const AIR_HORN_ID = 'UaUa_0qPPgc';
 const TWO_SECONDS = 2000;
 
 type SoundMap = {
@@ -31,10 +24,6 @@ type SoundMap = {
 }
 
 export class WelcomeTuneBot extends IBot<IRhythmBotConfig> {
-    helptext: string;
-    player: MediaPlayer;
-    status: BotStatus;
-
     constructor(
         config: IRhythmBotConfig
     ) {
@@ -50,13 +39,12 @@ export class WelcomeTuneBot extends IBot<IRhythmBotConfig> {
                 logs: '../bot.log',
             },
         });
-
-        this.helptext = helptext;
     }
 
     onRegisterDiscordCommands(map: CommandMap<(cmd: SuccessfulParsedMessage<Message>, msg: Message) => void>): void {
         const commandMap: { [key: string]: ICommand } = {
-            horn: new ForcePlayVideoCommand(this.player, AIR_HORN_ID),
+            horn: new PlaySoundFileCommand('airhorn_four.wav'),
+            badumtss: new PlaySoundFileCommand('badum_tss.wav')
         };
 
         const descriptions = Object
@@ -79,36 +67,14 @@ export class WelcomeTuneBot extends IBot<IRhythmBotConfig> {
                     return;
                 }
 
-                if (!this.player) {
-                    msg.channel.send(createErrorEmbed(`Error: no player found.`));
-                    return;
-                }
-
-                if (!this.player.connection) {
-                    try {
-                        await this.player.setConnection(msg.member.voice.channel);
-                    } catch (error) {
-                        msg.channel.send(createErrorEmbed(`Error: player ${msg.member.nickname} is not in a voice channel`));
-                        return;
-                    }
-                }
-
                 commandMap[key].execute(cmd, msg);
             })
         });
     }
 
-    parsedMessage(msg: SuccessfulParsedMessage<Message>) {
-        const handlers = this.commands.get(msg.command);
-        if (handlers) {
-            this.player.setChannel(msg.message.channel);
-        }
-    }
+    parsedMessage(msg: SuccessfulParsedMessage<Message>) {}
 
     onClientCreated(client: Client): void {
-        this.status = new BotStatus(client);
-        this.player = new MediaPlayer(this.config, this.status, this.logger);
-
         client.on('voiceStateUpdate', (oldVoiceState: VoiceState, newVoiceState: VoiceState) => {
             if (oldVoiceState.channelID) {
                 return;
