4cb7d98285613238e266fe24498358806f1684d2
Herr Scheissmann <wouternolten@gmail.com>
2022-02-13 16:29:22 +0100

    Prepare for dependency injection

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+export default {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  // An array of regexp pattern strings used to skip coverage collection
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/tests/"
+  ],
+
+  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+
+  // The test environment that will be used for testing
+  testEnvironment: "node",
+};
