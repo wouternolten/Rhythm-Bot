879c5f0714721c2dd0b15ec27c9ded656914ba05
Herr Scheissmann <wouternolten@gmail.com>
2022-02-19 16:10:43 +0100

    Make bot only responsible for executing commands

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
+import { ICommandMapFactory } from './command/ICommandMapFactory';
+import { CommandMapFactory } from './command/CommandMapFactory';
+import { MediaPlayer } from '../src/media';
+import { BotStatus } from './bot/bot-status';
+import { Client, Message, MessageReaction, User } from 'discord.js';
 import 'reflect-metadata';
 import * as fs from 'fs';
 import * as path from 'path';
 import { IMediaTypeProvider } from './mediatypes/IMediaTypeProvider';
 import { Container } from 'typedi';
 import { MediaTypeProvider } from './mediatypes/MediaTypeProvider';
-import { requireFile, projectDir, writeJson, ConsoleReader } from 'discord-bot-quickstart';
+import { requireFile, projectDir, writeJson, ConsoleReader, ParsedArgs, Interface, Logger } from 'discord-bot-quickstart';
 import { config as dotenv } from 'dotenv';
-import { MikroORM, IDatabaseDriver, Connection } from '@mikro-orm/core';
 import { IRhythmBotConfig, RhythmBot } from './bot';
-import { Playlist } from './media/playlist.model';
 import { WelcomeTuneBot } from './bot/welcometunebot';
 import winston, { createLogger, transports, format } from 'winston';
 
@@ -20,53 +23,59 @@ const lineFormat = printf(({ level, message, timestamp }) => {
 
 dotenv();
 
-export let ORM: MikroORM<IDatabaseDriver<Connection>>;
-
 (async () => {
     try {
-        const configPath = projectDir('../bot-config.json');
-        if (!fs.existsSync(configPath)) {
-            await writeJson({ discord: { token: '<BOT-TOKEN>' }, useWelcomeBot: false }, configPath);
+        let config: IRhythmBotConfig;
+
+        try {
+            config = await getConfig('../bot-config.json');
+        } catch (error) {
+            console.error({ configError: error });
+            process.exit(1);
         }
-        const config: IRhythmBotConfig = requireFile(configPath);
-
-        if (!!config && config.discord.token === '<BOT-TOKEN>') {
-            console.error('Invalid Token for the music bot - Create valid token in the Discord Developer Portal');
-            process.exit(0);
-        }
-
-        ORM = await MikroORM.init({
-            type: 'sqlite',
-            dbName: 'data/rhythm.db',
-            entities: [Playlist],
-        });
-        const migrator = ORM.getMigrator();
-        migrator.createMigration();
-        await migrator.up();
-
-        Container.set('logger', createLogger({
-            level: 'silly',
-            format: combine(
-                timestamp(),
-                lineFormat
-            ),
-            transports: [
-                new Console(),
-                new File({ filename: path.basename(config.directory.logs), dirname: path.dirname(config.directory.logs), maxsize: 1e+7 })
-            ]
-        }) as winston.Logger);
+        await createContainer(config);
 
         const mediaTypeProvider = Container.get(MediaTypeProvider) as IMediaTypeProvider;
+        const consoleReader = new ConsoleReader(Container.get('logger'));
+        
+        const client: Client = new Client();
+
+        try {
+            await client.login(config.discord.token);
+        } catch (error) {
+            console.error({ clientLoginError: error });
+            process.exit(1);
+        }
+
+        const botStatus = new BotStatus(client.user);
+        consoleReader
+            .commands
+            .on('exit', (args: ParsedArgs, rl: Interface) => {
+                client.destroy();
+                rl.close();
+            });
+        
+        consoleReader.listen();
+
+        Container.set('consoleReader', consoleReader);
+
+        const mediaPlayer = new MediaPlayer(config, botStatus, Container.get('logger'), mediaTypeProvider);
+        const logger: Logger = Container.get('logger') as Logger;
+        const commandMapFactory: ICommandMapFactory = new CommandMapFactory(mediaPlayer, config);
+
         const musicBot = new RhythmBot(
             config,
-            mediaTypeProvider,
-            Container.get('logger'),
-            new ConsoleReader(Container.get('logger'))
+            client.user,
+            mediaPlayer,
+            logger,
+            commandMapFactory.createMusicBotCommandsMap()
         );
 
-        musicBot.connect().then(() => {
-            musicBot.listen();
-        });
+        client.on('message', (msg: Message) => musicBot.handleMessage(msg));
+        client.on('messageReactionAdd', (reaction: MessageReaction, user: User) => musicBot.handleReaction(reaction, user));
+        client.on('ready', () => logger.debug('Bot online'));
+        client.on('disconnect', () => logger.debug('Bot Disconnected'));
+        client.on('error', (error: Error) => logger.error({ clientError: error }));
 
         if (config.useWelcomeBot) {
             const cloneConfig = {
@@ -88,3 +97,34 @@ export let ORM: MikroORM<IDatabaseDriver<Connection>>;
         console.error(error);
     }
 })();
+
+async function getConfig(configName: string): Promise<IRhythmBotConfig> {
+    const configPath = projectDir(configName);
+
+    if (!fs.existsSync(configPath)) {
+        await writeJson({ discord: { token: '<BOT-TOKEN>' }, useWelcomeBot: false }, configPath);
+    }
+
+    const config: IRhythmBotConfig = requireFile(configPath);
+
+    if (!!config && config.discord.token === '<BOT-TOKEN>') {
+        return Promise.reject('Invalid Token for the music bot - Create valid token in the Discord Developer Portal');
+    }
+
+    return config;
+}
+
+async function createContainer(config: IRhythmBotConfig): Promise<void> {
+    Container.set('logger', createLogger({
+        level: 'silly',
+        format: combine(
+            timestamp(),
+            lineFormat
+        ),
+        transports: [
+            new Console(),
+            new File({ filename: path.basename(config.directory.logs), dirname: path.dirname(config.directory.logs), maxsize: 1e+7 })
+        ]
+    }) as winston.Logger);
+}
+
