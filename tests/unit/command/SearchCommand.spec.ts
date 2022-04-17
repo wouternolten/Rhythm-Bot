b3cb3df01e799150c6351c56f80172e94c4a77c8
Herr Scheissmann <wouternolten@gmail.com>
2022-04-17 12:27:27 +0200

    Add unit tests for search command

--- tests/unit/command/SearchCommand.spec.ts
+++ tests/unit/command/SearchCommand.spec.ts
@@ -1,10 +1,11 @@
+import { MEDIA_TYPE_YOUTUBE } from './../../../src/mediatypes/MediaType';
 import { SuccessfulParsedMessage } from 'discord-bot-quickstart';
 import { SearchCommand } from './../../../src/command/SearchCommand';
 import { MediaPlayer } from './../../../src/media';
 import { IRhythmBotConfig } from './../../../src/bot/bot-config';
 import { IMediaItemHelper } from './../../../src/helpers/IMediaItemHelper';
-import { Message } from 'discord.js';
-import { createInfoEmbed } from '../../../src/helpers';
+import { Message, MessageEmbed } from 'discord.js';
+import { createEmbed, createInfoEmbed } from '../../../src/helpers';
 
 jest.mock('../../../src/helpers');
 
@@ -14,12 +15,22 @@ const player = {
     play: jest.fn()
 } as unknown as MediaPlayer;
 
-const mediaItemHelper = {} as unknown as IMediaItemHelper;
-const config = {} as unknown as IRhythmBotConfig;
+const mediaItemHelper = {
+    getMediaItemsForSearchString: jest.fn(),
+    getMediaItemForSearchString: jest.fn()
+} as IMediaItemHelper;
+
+const config = {
+    emojis: {
+        addSong: 'add_song_emoji'
+    }
+} as unknown as IRhythmBotConfig;
+
 let searchCommand: SearchCommand;
 
 const RICK_ASTLEY = 'Rick Astley';
 const NEVER_GONNA_GIVE_YOU_UP_YOUTUBE_LINK = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+const NEVER_GONNA_GIVE_YOU_UP = 'Never gonna give you up';
 
 const MESSAGE = {
     channel: {
@@ -52,26 +63,64 @@ it.each([undefined, null, ''])
         expect(createInfoEmbed).toHaveBeenCalledWith('Please input a song.');
     });
 
-it('Should directly add media and play if input is a YT url', async () => {
-    const cmd = { body: NEVER_GONNA_GIVE_YOU_UP_YOUTUBE_LINK } as SuccessfulParsedMessage<Message>;
 
-    (player.isPlaying as jest.Mock).mockReturnValue(false);
+describe('Youtube search', () => {
+    it('Should directly add media and play if input is a YT url', async () => {
+        const cmd = { body: NEVER_GONNA_GIVE_YOU_UP_YOUTUBE_LINK } as SuccessfulParsedMessage<Message>;
 
-    await searchCommand.execute(cmd, MESSAGE);
+        (player.isPlaying as jest.Mock).mockReturnValue(false);
 
-    expect(player.addMedia).toBeCalled();
-    expect(player.play).toBeCalled();
+        await searchCommand.execute(cmd, MESSAGE);
+
+        expect(player.addMedia).toBeCalled();
+        expect(player.play).toBeCalled();
+    });
+
+    it('Should directly add media and play if input is a YT url but player is already playing', async () => {
+        const cmd = { body: NEVER_GONNA_GIVE_YOU_UP_YOUTUBE_LINK } as SuccessfulParsedMessage<Message>;
+
+        (player.isPlaying as jest.Mock).mockReturnValue(true);
+
+        await searchCommand.execute(cmd, MESSAGE);
+
+        expect(player.addMedia).toBeCalled();
+        expect(player.play).not.toBeCalled();
+    });
 });
 
-it('Should directly add media and play if input is a YT url but player is already playing', async () => {
-    const cmd = { body: NEVER_GONNA_GIVE_YOU_UP_YOUTUBE_LINK } as SuccessfulParsedMessage<Message>;
+describe('Keyword search', () => {
+    const keywordCmd = { body: NEVER_GONNA_GIVE_YOU_UP } as SuccessfulParsedMessage<Message>;
 
-    (player.isPlaying as jest.Mock).mockReturnValue(true);
+    it('Should display a message when no results found', async () => {
+        (mediaItemHelper.getMediaItemsForSearchString as jest.Mock).mockReturnValue([]);
 
-    await searchCommand.execute(cmd, MESSAGE);
+        await searchCommand.execute(keywordCmd, MESSAGE);
 
-    expect(player.addMedia).toBeCalled();
-    expect(player.play).not.toBeCalled();
+        expect(createInfoEmbed).toBeCalled();
+    });
+
+    it('Should add found videos to channel', async () => {
+        (mediaItemHelper.getMediaItemsForSearchString as jest.Mock)
+            .mockReturnValue([
+                {
+                    type: MEDIA_TYPE_YOUTUBE,
+                    url: NEVER_GONNA_GIVE_YOU_UP_YOUTUBE_LINK
+                } 
+            ]);
+        
+        const placedMessage = {
+            react: jest.fn()
+        };
+
+        (createEmbed as jest.Mock).mockImplementation(() => {
+            return new MessageEmbed().setColor('#a600ff');
+        });
+        
+        (MESSAGE.channel.send as jest.Mock).mockResolvedValue(placedMessage);
+        
+        await searchCommand.execute(keywordCmd, MESSAGE);
+
+        expect(createEmbed).toBeCalled();
+        expect(placedMessage.react).toBeCalled();
+    });
 });
-
-it('Should send ')
