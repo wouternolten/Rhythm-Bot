fcfd93e298f299a7c0d85675f348058c3c183bdc
Herr Scheissmann <wouternolten@gmail.com>
2023-08-06 11:45:02 +0200

    Prettify the YTAPIHelper

--- src/helpers/YoutubeAPIHelper.ts
+++ src/helpers/YoutubeAPIHelper.ts
@@ -1,18 +1,17 @@
 import { MEDIA_TYPE_YOUTUBE } from './../mediatypes/MediaType';
 import { Logger } from 'winston';
-import { MediaItem } from "../media/MediaItem";
-import { IMediaItemHelper } from "./IMediaItemHelper";
+import { MediaItem } from '../media/MediaItem';
+import { IMediaItemHelper } from './IMediaItemHelper';
 import * as youtube from 'youtube-search-without-api-key';
 
 export class YoutubeAPIHelper implements IMediaItemHelper {
-    constructor(private readonly logger: Logger) {
-    }
+    constructor(private readonly logger: Logger) {}
 
     async getMediaItemForSearchString(searchString: string): Promise<MediaItem | null> {
         if (searchString === '') {
             return null;
         }
-        
+
         const returnValue = await this.getMediaItemsForSearchString(searchString, 1);
 
         if (!Array.isArray(returnValue)) {
@@ -29,21 +28,21 @@ export class YoutubeAPIHelper implements IMediaItemHelper {
 
         try {
             const youtubeSearchResults = await youtube.search(searchString);
-            
+
             if (!youtubeSearchResults || youtubeSearchResults.length === 0) {
                 return null;
             }
 
-            return youtubeSearchResults
-                .slice(0, limit)
-                .map(video => ({
+            return youtubeSearchResults.slice(0, limit).map(
+                (video) =>
+                    ({
                         type: MEDIA_TYPE_YOUTUBE,
                         url: video.snippet.url,
                         name: video.snippet.title,
                         duration: video.duration_raw,
-                        imageUrl: video.snippet?.thumbnails?.url
+                        imageUrl: video.snippet?.thumbnails?.url,
                     } as MediaItem)
-                );
+            );
         } catch (errorSearchingYoutube) {
             this.logger.error(JSON.stringify({ errorSearchingYoutube }));
             return null;
