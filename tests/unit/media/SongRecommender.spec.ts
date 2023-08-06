bd0a0a50557542845b5c0b9e35dbca6ffa674621
Herr Scheissmann <wouternolten@gmail.com>
2023-08-06 20:56:31 +0200

    Fix reuestor in song recommender test

--- tests/unit/media/SongRecommender.spec.ts
+++ tests/unit/media/SongRecommender.spec.ts
@@ -1,25 +1,21 @@
-import { MediaItem } from './../../../src/media/MediaItem';
-import { SpotifyAPIHelper } from './../../../src/helpers/SpotifyAPIHelper';
-import { SongRecommender } from './../../../src/media/SongRecommender';
 import { mock, MockProxy } from 'jest-mock-extended';
-import { IMediaItemHelper } from '../../../src/helpers/IMediaItemHelper';
 import { Logger } from 'winston';
+import { IMediaItemHelper } from '../../../src/helpers/IMediaItemHelper';
+import { SpotifyAPIHelper } from './../../../src/helpers/SpotifyAPIHelper';
+import { MediaItem } from './../../../src/media/MediaItem';
+import { SongRecommender } from './../../../src/media/SongRecommender';
 
 let songRecommender: SongRecommender;
 let spotifyApiHelper: MockProxy<SpotifyAPIHelper>;
 let mediaItemHelper: MockProxy<IMediaItemHelper>;
-let logger: MockProxy<Logger>
+let logger: MockProxy<Logger>;
 
 beforeEach(() => {
     spotifyApiHelper = mock<SpotifyAPIHelper>();
     mediaItemHelper = mock<IMediaItemHelper>();
     logger = mock<Logger>();
-    
-    songRecommender = new SongRecommender(
-        spotifyApiHelper,
-        mediaItemHelper,
-        logger
-    );
+
+    songRecommender = new SongRecommender(spotifyApiHelper, mediaItemHelper, logger);
 });
 
 describe('recommendNextSong()', () => {
@@ -29,46 +25,40 @@ describe('recommendNextSong()', () => {
 
     it('Should search for track and artist when lastPlayedSong has them', async () => {
         const lastPlayedSong = {
-            name: "Rick Astley - Never gonna give you up"
+            name: 'Rick Astley - Never gonna give you up',
         } as MediaItem;
 
         await songRecommender.recommendNextSong(lastPlayedSong);
 
-        expect(spotifyApiHelper.getSpotifyIDForSong).toHaveBeenCalledWith(
-            "Never gonna give you up",
-            "Rick Astley"
-        );
+        expect(spotifyApiHelper.getSpotifyIDForSong).toHaveBeenCalledWith('Never gonna give you up', 'Rick Astley');
     });
 
     it('Should search for full name when lastPlayedSong does not have hyphen', async () => {
         const lastPlayedSong = {
-            name: "Rick Astley Never gonna give you up"
+            name: 'Rick Astley Never gonna give you up',
         } as MediaItem;
 
         await songRecommender.recommendNextSong(lastPlayedSong);
 
         expect(spotifyApiHelper.getSpotifyIDForSong).toHaveBeenCalledWith(
-            "Rick Astley Never gonna give you up",
+            'Rick Astley Never gonna give you up',
             undefined
         );
     });
 
     it('Should search for track and artist when lastPlayedSong has them with brackets added', async () => {
         const lastPlayedSong = {
-            name: "Rick Astley - Never gonna give you up (Official video)"
+            name: 'Rick Astley - Never gonna give you up (Official video)',
         } as MediaItem;
 
         await songRecommender.recommendNextSong(lastPlayedSong);
 
-        expect(spotifyApiHelper.getSpotifyIDForSong).toHaveBeenCalledWith(
-            "Never gonna give you up",
-            "Rick Astley"
-        );
+        expect(spotifyApiHelper.getSpotifyIDForSong).toHaveBeenCalledWith('Never gonna give you up', 'Rick Astley');
     });
 
     it('Should log and return null when spotify helper throws error', async () => {
         const lastPlayedSong = {
-            name: "Rick Astley - Never gonna give you up (Official video)"
+            name: 'Rick Astley - Never gonna give you up (Official video)',
         } as MediaItem;
 
         const error = new Error();
@@ -83,18 +73,23 @@ describe('recommendNextSong()', () => {
 
     it('Should return mediaItem from helper when found', async () => {
         const lastPlayedSong = {
-            name: "Rick Astley - Never gonna give you up (Official video)"
+            name: 'Rick Astley - Never gonna give you up (Official video)',
+        } as MediaItem;
+
+        const mediaItem = {
+            name: 'Some uber awesome name',
+            url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
         } as MediaItem;
 
         const expected = {
-            name: "Some uber awesome name",
-            url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-        } as MediaItem;
+            ...mediaItem,
+            requestor: 'Autoplay',
+        };
 
         spotifyApiHelper.getSpotifyIDForSong.mockResolvedValue('123');
         spotifyApiHelper.getRecommendationForTrack.mockResolvedValue('some-string');
-        mediaItemHelper.getMediaItemForSearchString.mockResolvedValue(expected);
+        mediaItemHelper.getMediaItemForSearchString.mockResolvedValue(mediaItem);
 
         expect(await songRecommender.recommendNextSong(lastPlayedSong)).toEqual(expected);
-    })
+    });
 });
