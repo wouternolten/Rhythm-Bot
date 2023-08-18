c492cd5606618f3abc4f71225679a9825b34b70b
Herr Scheissmann <wouternolten@gmail.com>
2023-08-18 13:48:36 +0200

    Fix link between audio player and event bus
    For in loops for enums in emitters apparently do not work. own statuses.

--- src/helpers/EventBus.ts
+++ src/helpers/EventBus.ts
@@ -1,3 +1,8 @@
 import { EventEmitter } from 'events';
 
+export enum AudioEventBusStatus {
+    AudioPlayerIdle = 'audioPlayer.idle',
+    AudioPlayerError = 'audioPlayer.error',
+}
+
 export class AudioEventBus extends EventEmitter {}
